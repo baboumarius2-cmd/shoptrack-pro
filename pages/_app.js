df058508bc249bc8e29d91ac930c4a5377d9d400
baboumarius2-cmd <baboumarius2@gmail.com>
2026-06-30 17:44:18 +0000

    apk

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
 import "../styles/globals.css";
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+        <title>Yah-ni Store</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
